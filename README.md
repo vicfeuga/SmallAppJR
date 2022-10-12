a0ee9863534be4762645a4e3c7bceba2db63b2fa
vicfeuga <vicfeuga@orange.fr>
2022-10-12 16:39:32 +0200

    newCommit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SmallApp
+A travers ce travail, j'ai appris à mieux manipuler GitHub.
+
+J'ai mis en place une application Angular qui affiche le message "HelloWorld" sur la version master
+J'ai fait en sorte que les pulls requests sur le master passent via un check 
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'hello-world'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('hello-world');
+  });
+  it(`should multiply`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.multiply(2,3)).toEqual(6);
+  });
+});
